f460beadea3a2e998bc05ea78f4f78d515bc494b
Piero Avola <117577483+pieroavola@users.noreply.github.com>
2023-04-22 16:58:48 +0200

    Prepare Maven Central Publication

--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -1,6 +1,9 @@
+import java.net.URI
+
 plugins {
   `java-library`
   `maven-publish`
+  signing
   kotlin("jvm")
   id("org.jetbrains.dokka")
 }
@@ -14,6 +17,7 @@ java {
   targetCompatibility = JavaVersion.VERSION_17
 
   withSourcesJar()
+  withJavadocJar()
 
   registerFeature("logging") {
     usingSourceSet(sourceSets["main"])
@@ -43,11 +47,80 @@ dependencies {
 }
 
 publishing {
-  publications {
-    create<MavenPublication>("utils") {
-      from(components["java"])
+
+  val mavenUser: String by project
+  val mavenPassword: String by project
+
+  repositories {
+
+    maven {
+      name = "mavenCentralSnapshots"
+      url = URI("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+      credentials {
+        username = mavenUser
+        password = mavenPassword
+      }
+
+      maven {
+        name = "mavenCentral"
+        url = URI("https://s01.oss.sonatype.org/content/repositories/releases/")
+        credentials {
+          username = mavenUser
+          password = mavenPassword
+        }
+      }
     }
   }
+
+  publications {
+    create<MavenPublication>("utils") {
+
+      from(components["java"])
+
+      artifactId = "utils"
+
+      pom {
+        name.set("ShortKt Utils")
+        description.set("Useful additions to the Kotlin standard library")
+        url.set("https://github.com/pieroavola/shortkt")
+
+        licenses {
+          license {
+            name.set("The Apache License, Version 2.0")
+            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+          }
+        }
+
+        developers {
+          developer {
+            id.set("pavola")
+            name.set("Piero Avola")
+            email.set("me@pieroavola.de")
+            organization.set("")
+            organizationUrl.set("https://github.com/pieroavola/")
+          }
+        }
+
+        scm {
+          connection.set("scm:git:git://github.com/pieroavola/shortkt.git")
+          developerConnection.set("scm:git:git://github.com/pieroavola/shortkt.git")
+          url.set("https://github.com/pieroavola/shortkt/tree/main/utils")
+        }
+      }
+    }
+  }
+}
+
+signing {
+  useGpgCmd()
+  sign(publishing.publications["utils"])
+}
+
+tasks.named<Jar>("javadocJar") {
+
+  dependsOn("dokkaJavadoc")
+  from("$buildDir/dokka/javadoc")
+  archiveClassifier.set("javadoc")
 }
 
 tasks.test {
